df1f372eede399d47716f1581d81a03bfbac2b92
Mahmoud Abdelkader <Mahmoud@domain.local>
2022-03-30 22:37:56 -0300

    clean up files v3

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,6 @@ export default function Custom404() {
                     <div className="col-md-8">
                         <div className="headline mt-5 mb-0">
                             <div className="headline-content text-center">
-                            <Image src="/images/404-1.svg"  className="img-fluid mb-5" width={300} height={200} alt="" />
                             <h1 className="headline-title display-3">Page <span className="text-danger">NOT</span> found!</h1>
                             <p className="headline-subtitle">The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved.</p>
                            <p className="pt-5"><Link href="/"><a  style={{borderBottom: '1px solid #212529'}}>Back home</a></Link></p>
@@ -25,9 +24,7 @@ export default function Custom404() {
                 </div>
                 
             </div>
-        </header>
-        <Cta title={"We guide users to their best experiences."} btn={"Let's Get Started"}/>
-        
+        </header>        
         </>
         </Layout>
     )
